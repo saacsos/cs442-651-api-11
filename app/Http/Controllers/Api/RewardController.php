b36674507a015a36f65816b5eb31311f2e10289d
Saac Sornchai <sornchai.la@ku.th>
2022-08-30 09:58:02 +0700

    Route API Resource

---
+++ app/Http/Controllers/Api/RewardController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Reward;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class RewardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $rewards = Reward::get();
+        return $rewards;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $reward = new Reward();
+        $reward->name = $request->get('name');
+        $reward->detail = $request->get('detail');
+        $reward->point = $request->get('point');
+        $reward->total_amount = $request->get('total_amount');
+        $reward->balance = $request->get('balance');
+        $reward->is_active = $request->get('is_active');
+        if ($reward->save()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Reward saved successfully with id ' . $reward->id,
+                'reward_id' => $reward->id
+            ], Response::HTTP_CREATED);
+        }
+        return response()->json([
+            'success' => false,
+            'message' => 'Reward saved failed'
+        ], Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Reward  $reward
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reward $reward)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Reward  $reward
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reward $reward)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Reward  $reward
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reward $reward)
+    {
+        //
+    }
+}
